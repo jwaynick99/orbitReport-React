c9b1fd1cf9e3fdb615df45bb8247ae83bea7938e
jwaynick99 <jwaynick99@gmail.com>
2024-08-20 23:23:36 -0500

    GA6 completed w/ styling

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,31 @@
+import "./styling.css";
+
 const Table = ({ sat }) => {
-  return (
-    <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
-  );
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Type Of Sattelite</th>
+                    <th>Launch Date</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {sat.map((data, id) => {
+                    return (
+                        <tr key={id}>
+                            <td>{data.name}</td>
+                            <td>{data.type}</td>
+                            <td>{data.launchDate}</td>
+                            {data.operational === true && <td>Active</td>}
+                            {data.operational === false && <td>Inactive</td>}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 };
 
 export default Table;
